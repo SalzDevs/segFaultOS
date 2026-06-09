034dc6ad6c619f9276b7b659f062cf5a346a9d72
Ricardo <ricardoceia@MacBook-Air-de-Ricardo.local>
2026-06-09 16:32:24 -0700

    Implement initial page table structure with basic initialization

--- main.c
+++ main.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <stdint.h>
 
-__attribute__((aligned(16))) uint8_t stack[4096];
+#define PAGE_SIZE 4096
+#define NUM_PAGES 8
+
+__attribute__((aligned(16))) uint8_t physical_ram[PAGE_SIZE * NUM_PAGES];
 
 int main(){
-  for (int i = 0; i < 4096; i++){
-    printf("%p\n",&stack[i]);
+  int page_table[NUM_PAGES];
+  printf("each page_table elem occupies: %lu bits\n",sizeof(int)*8);
+
+  for (int i = 0; i < NUM_PAGES; i++){
+    page_table[i] = -1;
+    printf("current memory:%p current val:%d\n",&page_table[i],page_table[i]);
   }
-  printf("first:%p\n",&stack[0]);
-  printf("last:%p\n",&stack[4096-1]);
   return 0;
 }
