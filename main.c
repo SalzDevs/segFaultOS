2ad5a2ee170e94b1aa7e473aa5481124a38dc61b
Ricardo <a49000@alunos.isel.pt>
2026-06-11 14:46:41 +0100

    func to translate virtual addr to physical ones

--- main.c
+++ main.c
@@ -6,13 +6,36 @@
 
 __attribute__((aligned(16))) uint8_t physical_ram[PAGE_SIZE * NUM_PAGES];
 
+uint64_t translate(uint64_t virtual_addr){
+  int vpn = virtual_addr/PAGE_SIZE;
+  int offset = virtual_addr%PAGE_SIZE;
+
+  if (page_table[vpn] == -1){
+    printf("Hardware Page Fault! Virtual Page %d not mapped",vpn);
+    return -1;
+  }
+
+  int ppn = page_table[vpn];
+  return ppn * PAGE_SIZE + offset;
+}
+
 int main(){
   int page_table[NUM_PAGES];
-  printf("each page_table elem occupies: %lu bits\n",sizeof(int)*8);
 
   for (int i = 0; i < NUM_PAGES; i++){
     page_table[i] = -1;
-    printf("current memory:%p current val:%d\n",&page_table[i],page_table[i]);
   }
+
+  page_table[3] = 5;
+  printf("OS mapped virtual page 3-> physical page 5\n");
+  
+  uint64_t vaddr = 0x3070;
+  uint64_t paddr = translate(vaddr,page_table);
+
+  if (paddr != (uint64_t)-1){
+    printf("[Hardware] Virtual 0x%lx → Physical 0x%lx\n", vaddr, paddr);
+    physical_ram[paddr] = 42;  
+  }
+  
   return 0;
 }
